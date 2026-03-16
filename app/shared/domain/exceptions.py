0bc0aae072dc70c7a32d900d1ac38e36336a188d
WayupKG <adikgk232323@gmail.com>
2026-03-16 12:50:28 +0600

    refactor: streamline `BaseCustomException` by updating attributes, enhancing error handling with `ErrorDetail`, and optimizing initialization

--- app/shared/domain/exceptions.py
+++ app/shared/domain/exceptions.py
@@ -1,42 +1,34 @@
-from typing import Any
-
 from fastapi import status
+
+from app.shared.presentation.schemas import ErrorDetail
 
 
 class BaseCustomException(Exception):
-    """
-    Базовый класс для всех ошибок приложения.
-    Не зависит от фреймворка.
-    """
-
     status_code: int = status.HTTP_400_BAD_REQUEST
-    detail: str = "Неизвестная ошибка бизнес-логики"
+    error_code: str = "BaseCustomException"
+    detail: str = "Ошибка бизнес-логики"
 
     def __init__(
         self,
-        detail: str | None = None,
         error_code: str | None = None,
+        detail: str | None = None,
         attr: str | None = None,
-        errors: list[dict[str, Any]] | None = None,
-        status_code: int = 400,
+        errors: list[dict[str, str | int]] | None = None,
+        status_code: int | None = None,
     ) -> None:
-        if status_code:
-            self.status_code = status_code
-        if detail:
-            self.detail = detail
-
-        self.error_code = error_code or self.__class__.__name__
-        self.attr = attr
+        self.status_code = status_code or self.status_code
+        self.detail = detail or self.detail
+        self.error_code = error_code or self.error_code
 
         if errors:
             self.errors = errors
         else:
             self.errors = [
-                {
-                    "error_code": self.error_code,
-                    "detail": self.detail,
-                    "attr": self.attr,
-                }
+                ErrorDetail(
+                    error_code=self.error_code,
+                    detail=self.detail,
+                    attr=attr,
+                ).model_dump(),
             ]
 
         super().__init__(self.detail)
